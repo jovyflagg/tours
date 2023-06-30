14f752ce43281e7f3a127babdf239d5e3040a581
Jovy Cortez <jovycortez11@gmail.com>
2023-06-30 16:54:05 -0400

    Adding some CSS Styling and App Logic

--- src/App.js
+++ src/App.js
@@ -1,10 +1,55 @@
+import { useEffect, useState } from 'react';
 import Tours from './Tours';
 import { client } from './axios'
+import Loading from './Loading';
 
 function App() {
+
+  const[tours,setTours] = useState([]);
+  const [loading,setLoading] = useState(true);
+
+  const fetchTours = async () => {
+    setLoading(true);
+    try{
+      const response = await client.get('');
+      setLoading(false);
+      setTours(response.data)
+    } catch(error){
+      setLoading(false)
+      console.log("FETCHTOURS() METHOD ERROR MESSAGE: " + error)
+    }
+  }
+
+  useEffect(() => {
+    fetchTours();
+  })
+
+  if (loading) {
+    return(
+      <main>
+        <Loading />
+      </main>
+    );
+  }
+
+  const removeTour = (id) => {
+    setTours(tours.filter((tour)=> tour.id !== id))
+  }
+
+  if (tours.length === 0) {
+    return (
+      <main>
+        <div className='title'>
+          <h2>no tours left</h2>
+          <button className='btn' onClick={fetchTours}>refresh</button>
+        </div>
+      </main>
+    )
+  }
+
   return (
     <div className="App">
-     <Tours />
+     <Tours tours={tours} removeTour={removeTour} />
     </div>
   );
 }
